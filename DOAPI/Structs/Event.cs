cd726aa7235485ec69e9fb1684dde2b8f08bcd23
Jamie Hankins <jamiehankin@gmail.com>
2014-03-11 11:42:32 +0000

    Event structure

---
+++ DOAPI/Structs/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalOcean.Structs
+{
+    public class Event
+    {
+        public string status { get; set; }
+        public int event_id { get; set; }
+    }
+
+    /// <summary>
+    /// This is returned from most deletion events and only has one attribute: status
+    /// </summary>
+    public class InstantEvent
+    {
+        public string status { get; set; }
+    }
+}
