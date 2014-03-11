761fb9376e696bc017ce2352bd2272b03908d70f
Jamie Hankins <jamiehankin@gmail.com>
2014-03-11 10:32:50 +0000

    Make way for the extended information which can be requested using the Show Droplet request.

--- DOAPI/Structs/Droplets.cs
+++ DOAPI/Structs/Droplets.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DigitalOcean.Structs
 {
-        public class Droplet
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-            public int image_id { get; set; }
-            public int size_id { get; set; }
-            public int region_id { get; set; }
-            public bool backups_active { get; set; }
-            public string ip_address { get; set; }
-            public object private_ip_address { get; set; }
-            public bool locked { get; set; }
-            public string status { get; set; }
-            public string created_at { get; set; }
-        }
+    public class Droplet
+    {
+        public int id { get; set; }
+        public int image_id { get; set; }
+        public string name { get; set; }
+        public int region_id { get; set; }
+        public int size_id { get; set; }
+        public bool backups_active { get; set; } 
+        public List<object> backups { get; set; } //extended
+        public List<object> snapshots { get; set; } //extended
+        public string ip_address { get; set; }
+        public object private_ip_address { get; set; }
+        public bool locked { get; set; }
+        public string status { get; set; }
+        public string created_at { get; set; }
+    }
 
-        public class Droplets
-        {
-            public string status { get; set; }
-            public List<Droplet> droplets { get; set; }
-        }
+    public class Droplets
+    {
+        public string status { get; set; }
+        public List<Droplet> droplets { get; set; }
+    }
 }
