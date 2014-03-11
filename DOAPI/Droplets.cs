4285d61626a6ff06902e67db0fc11d42dcd0bc66
Jamie Hankins <jamiehankin@gmail.com>
2014-03-11 14:04:47 +0000

    Add droplet methods

--- DOAPI/Droplets.cs
+++ DOAPI/Droplets.cs
@@ -38,5 +38,98 @@ namespace DigitalOcean
             request.AddParameter("ssh_key_ids", ssh_key_ids);
             return Execute<CreateDropletStatus>(request);
         }
+
+        public virtual Event RestartDroplet(int server_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/reboot/";
+            return Execute<Event>(request);
+        }
+
+        public virtual Event PowercycleDroplet(int server_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/power_cycle/";
+            return Execute<Event>(request);
+        }
+
+        public virtual Event ShutdownDroplet(int server_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/shutdown/";
+            return Execute<Event>(request);
+        }
+
+        public virtual Event PoweroffDroplet(int server_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/power_off/";
+            return Execute<Event>(request);
+        }
+
+        public virtual Event PoweronDroplet(int server_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/power_on/";
+            return Execute<Event>(request);
+        }
+
+        public virtual Event ResetPasswordDroplet(int server_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/password_reset/";
+            return Execute<Event>(request);
+        }
+
+        public virtual Event ResizeDroplet(int server_id, int size_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/resize/";
+            request.AddParameter("size_id", size_id);
+            return Execute<Event>(request);
+        }
+
+        public virtual Event SnapshotDroplet(int server_id, string snapshot_name = "")
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/snapshot/";
+            if (snapshot_name != "")
+            {
+                request.AddParameter("name", snapshot_name);
+            }
+            return Execute<Event>(request);
+        }
+
+        public virtual Event RestoreDroplet(int server_id, int image_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/restore/";
+            request.AddParameter("image_id ", image_id);
+            return Execute<Event>(request);
+        }
+
+        public virtual Event RebuildeDroplet(int server_id, int image_id)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/rebuild/";
+            request.AddParameter("image_id ", image_id);
+            return Execute<Event>(request);
+        }
+
+        public virtual Event RenameDroplet(int server_id, int name)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/rename/";
+            request.AddParameter("name ", name);
+            return Execute<Event>(request);
+        }
+
+        public virtual Event DestroyDroplet(int server_id, bool scrub_data = false)
+        {
+            var request = new RestRequest();
+            request.Resource = "droplets/" + server_id + "/destroy/";
+            request.AddParameter("scrub_data ", scrub_data);
+            return Execute<Event>(request);
+        }
     }
 }
